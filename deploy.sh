22e103ebcb36dbea01dfa4068642df9640a62d4f
Rowin <rowin.mol@youngcapital.nl>
2021-04-29 23:28:04 +0200

    fix: bug with not deploying the application to  digital ocean

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ read VERSION
 
 docker build -t rowin1125/rowdit:$VERSION .
 docker push rowin1125/rowdit:$VERSION
-ssh root@188.166.68.218 "docker pull rowin1125/rowdit:$VERSION && docker tag rowin1125/rowdit:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+ssh root@188.166.68.218 "docker pull rowin1125/rowdit:$VERSION && docker tag rowin1125/rowdit:$VERSION dokku/api:$VERSION && dokku tags:deploy api $VERSION"
